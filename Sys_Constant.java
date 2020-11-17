908b84c37ed64bbec8894f73754fb6fe14af64a6
Thealchemist20 <48990504+Thealchemist20@users.noreply.github.com>
2020-11-17 12:33:23 +0530

    Generic Package

---
+++ Sys_Constant.java
@@ -0,0 +1,12 @@
+package generic;
+
+public interface Sys_Constant {
+
+	String URL = "https://online.actitime.com/hosegoods/login.do";
+    long ITO  = 10;
+    long PAGE_LOAD_OUT=60;
+    String TEST_DATA_PATH = "../mail.cock.li/TestData/Data.xlsx";
+    
+	
+	
+}
